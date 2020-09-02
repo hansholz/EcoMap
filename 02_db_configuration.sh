ca362b43a122e5fca856a08d56c294236989c6dd
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-02 08:42:33 +0300

    Create 02_db_configuration.sh

---
+++ 02_db_configuration.sh
@@ -0,0 +1,11 @@
+sudo sed -i 's/address/database-ecomap.cjc2siunmkts.us-east-2.rds.amazonaws.com/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
+sudo sed -i 's/portdb/3306/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
+sudo sed -i 's/address/database-ecomap.cjc2siunmkts.us-east-2.rds.amazonaws.com/g' /home/ec2-user/EcoMap/ecomap/etc/db.conf
+sudo sed -i 's/portdb/3306/g' /home/ec2-user/EcoMap/ecomap/etc/db.conf
+sudo sed -i 's/address/database-ecomap.cjc2siunmkts.us-east-2.rds.amazonaws.com/g' /home/ec2-user/EcoMap/ecomap/db/ecomap/dump.sh
+sudo sed -i 's/portdb/3306/g' /home/ec2-user/EcoMap/ecomap/db/ecomap/dump.sh
+sudo sed -i 's/username/admin/g' /home/ec2-user/EcoMap/ecomap/db/ecomap/dump.sh
+sudo sed -i 's/userpass/amoremiosa/g' /home/ec2-user/EcoMap/ecomap/db/ecomap/dump.sh
+sudo sed -i 's/dbname/ecomap_db/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
+sudo sed -i 's/dbname/ecomap_db/g' /home/ec2-user/EcoMap/ecomap/etc/db.conf
+sudo sed -i 's/dbname/ecomap_db/g' /home/ec2-user/EcoMap/ecomap/db/ecomap/dump.sh
