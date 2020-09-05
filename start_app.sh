9a65a5fa16d4670cb9eb2269fdde2866fc3a93e2
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-05 23:57:50 +0300

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-yum -y update
-amazon-linux-extras -y install epel
-yum -y install git python2-pip python-devel httpd mod_wsgi mysql-devel gcc MySQL-python memcached libxslt-devel libxml2-devel
-pip install --upgrade setuptools
-pip install --upgrade pip
-pip install --ignore-installed -r requirements.txt
-
 cp ./httpd_configs/httpd.conf /etc/httpd/conf/httpd.conf
 cp ./httpd_configs/ecomap.conf /etc/httpd/conf.d/ecomap.conf
 chown -R apache /home/ec2-user/
