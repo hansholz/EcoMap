02bc711e6a80b18f99af5bafce6ec9a533ad9a99
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-05 22:05:38 +0300

    Update and rename 02_db_configuration.sh to 02_start_app.config

--- .ebextensions/02_db_configuration.sh
+++
@@ -1,4 +0,0 @@
-sudo sed -i 's/address/database-ecomap.cjc2siunmkts.us-east-2.rds.amazonaws.com/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
-sudo sed -i 's/portdb/3306/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
-sudo sed -i 's/dbname/ecomap_db/g' /home/ec2-user/EcoMap/ecomap/etc/_configvars.conf
-
