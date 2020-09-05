ca08818cd723bacb4540248ff288ae1c8d2d42d6
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-05 21:08:35 +0300

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -5,10 +5,8 @@ amazon-linux-extras -y install epel
 yum -y install git python2-pip python-devel httpd mod_wsgi mysql-devel gcc MySQL-python memcached libxslt-devel libxml2-devel
 pip install --upgrade setuptools
 pip install --upgrade pip
-
-git clone https://github.com/hansholz/EcoMap.git
-cd EcoMap
 pip install --ignore-installed -r requirements.txt
+
 cp ./httpd_configs/httpd.conf /etc/httpd/conf/httpd.conf
 cp ./httpd_configs/ecomap.conf /etc/httpd/conf.d/ecomap.conf
 chown -R apache /home/ec2-user/
