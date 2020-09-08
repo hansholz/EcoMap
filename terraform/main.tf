0c61c6c465d1588cd413746324bd6a48c0a25ea5
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-08 21:29:18 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@
 # Region
 #------------------------------------------------
 
-provider "aws" {
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
-  region     = var.aws_region
-}
+#provider "aws" {
+#  access_key = var.ACCESS_KEY
+#  secret_key = var.SECRET_KEY
+#  region     = var.aws_region
+#}
 
 #------------------------------------------------
 # EB
