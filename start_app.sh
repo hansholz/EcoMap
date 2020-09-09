941ef7c9ccfe8fcd802582cb267e442026501f17
hansholz <48967084+hansholz@users.noreply.github.com>
2020-09-09 06:25:44 +0300

    Delete start_app.sh

--- start_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp ./httpd_configs/httpd.conf /etc/httpd/conf/httpd.conf
-cp ./httpd_configs/ecomap.conf /etc/httpd/conf.d/ecomap.conf
-chown -R apache /home/ec2-user/
-chown -R apache:apache /home/ec2-user/EcoMap/
-service httpd start
